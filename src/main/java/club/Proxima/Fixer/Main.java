3737ecaeb8ad3848190bfddab91a75f38acda4c6
Anton Fresko <mine.2d2d@gmail.com>
2022-07-12 05:35:57 +0300

    v0.0.1

---
+++ src/main/java/club/Proxima/Fixer/Main.java
@@ -0,0 +1,15 @@
+package club.Proxima.Fixer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("&9[ProximaExploitFixer] &3Enable!");
+    }
+    
+    @Override
+    public void onDisable() {
+        
+    }
+}
