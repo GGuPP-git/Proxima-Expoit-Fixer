5cef15e0033d211d8d00fce6b58bb13115fbed6a
Anton Fresko <mine.2d2d@gmail.com>
2022-07-19 20:08:25 +0300

    complite base

--- src/main/java/club/Proxima/Fixer/Main.java
+++ src/main/java/club/Proxima/Fixer/Main.java
@@ -1,15 +1,62 @@
 package club.Proxima.Fixer;
 
+
+//Other func
+import club.Proxima.Fixer.fixes.*;
+
+import org.bstats.bukkit.Metrics;
+import org.bukkit.Bukkit;
+import org.bukkit.Chunk;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.ShulkerBox;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.*;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BlockStateMeta;
+import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.inventory.meta.SpawnEggMeta;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Main extends JavaPlugin {
+import java.awt.*;
+import java.util.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+public class Main extends JavaPlugin implements Listener {
+    public PluginManager pluginManager;
+
     @Override
     public void onEnable() {
-        getLogger().info("&9[ProximaExploitFixer] &3Enable!");
-    }
-    
-    @Override
-    public void onDisable() {
-        
+        pluginManager = getServer().getPluginManager();
+
+        //here u must write name func from fixes folder (new namefunc(this))
+        load();
+
+        if (getConfig().getBoolean("DisableAllProtocolLib")) {
+            getLogger().info("You specified to disable all ProtocolLib patches.");
+        } else {
+            if (getServer().getPluginManager().getPlugin("ProtocolLib") != null) {
+                getLogger().info("Detected ProtocolLib!");
+                Parash.protocolLibWrapper(this);
+            } else {
+                getLogger().warning("Did not detect ProtocolLib, disabling packet patches");
+                Bukkit.getScheduler().runTaskTimer(this, () -> getLogger().warning("ProtocolLib plugin not detected, many gamebreaking exploits will not be patched. Download at https://www.spigotmc.org/resources/protocollib.1997/"), 200L, 1200L);
+            }
+        }
+    }    
+
+
+    private void load(Listener... list) {
+        pluginManager.registerEvents(this, this);
+        for (Listener listener : list) {
+            pluginManager.registerEvents(listener, this);
+            getLogger().info("Loaded class " + listener.getClass().getSimpleName());
+        }
     }
 }
+
---
+++ src/main/java/club/Proxima/Fixer/fixes/Parash.java
@@ -0,0 +1,78 @@
+package club.Proxima.Fixer.fixes;
+
+import com.comphenix.protocol.PacketType;
+import org.bukkit.ChatColor;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.ListenerPriority;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.wrappers.BlockPosition;
+import com.comphenix.protocol.wrappers.EnumWrappers;
+import io.papermc.lib.PaperLib;
+import club.Proxima.Fixer.Main;
+import org.bukkit.Bukkit;
+import org.bukkit.Chunk;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.*;
+
+public class Parash {
+    public static void protocolLibWrapper(Main plugin) {
+        final Map<UUID, Integer> levels = new HashMap<>();
+        final Map<UUID, Integer> boatLevels = new HashMap<>();
+        ProtocolManager protocolManager = ProtocolLibrary.getProtocolManager();
+
+
+        if (plugin.getConfig().getBoolean("PreventPacketFly")) {
+            protocolManager.addPacketListener(
+                    new PacketAdapter(plugin, ListenerPriority.HIGHEST, PacketType.Play.Client.TELEPORT_ACCEPT) {
+                        @Override
+                        public void onPacketReceiving(PacketEvent event) {
+                            if (event.isPlayerTemporary()) {
+                                return;
+                            }
+
+                            Player e = event.getPlayer();
+                            Location l = e.getLocation();
+                            int relX = l.getBlockX() & 0xF; // keep the 4 least significant bits, range 0-15
+                            int relY = l.getBlockY();
+                            int relZ = l.getBlockZ() & 0xF;
+
+                            Chunk chunk = PaperLib.getChunkAtAsync(l, false).join();
+                            if (chunk != null
+                                    && (relY <= 0 || chunk.getBlock(relX, relY - 1, relZ).getType() == Material.AIR)
+                                    && !e.isGliding()
+                                    && !e.isInsideVehicle()) {
+                                Integer level = levels.get(e.getUniqueId());
+                                if (level != null) {
+                                    if (level > plugin.getConfig().getInt("MaxTeleportPacketsPer10Seconds")) {
+                                        event.setCancelled(true);
+                                        if (true) {
+                                            Bukkit.getScheduler().runTask(this.plugin, new Runnable() { 
+                                                public void run() { 
+                                                    e.kickPlayer(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("PacketFlyKikcMessage")));
+                                                }
+                                            });
+                                        }
+                                        if (plugin.getConfig().getBoolean("LogPacketFlyEvents")) {
+                                            plugin.getLogger().warning(e.getName() + " prevented from packetflying");
+                                        }
+                                    } else {
+                                        levels.merge(e.getUniqueId(), 1, Integer::sum);
+                                        Bukkit.getServer().getScheduler().runTaskLater(plugin, () -> levels.put(e.getUniqueId(), levels.get(e.getUniqueId()) - 1), 200L);
+                                    }
+                                } else {
+                                    levels.put(e.getUniqueId(), 1);
+                                    Bukkit.getServer().getScheduler().runTaskLater(plugin, () -> levels.put(e.getUniqueId(), levels.get(e.getUniqueId()) - 1), 200L);
+                                }
+                            }
+                        }
+                    });
+        }
+    }
+}
